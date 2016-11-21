b1a96b0a3bec2ebc693e02a588d44ae3d149b568
Eduardo RC <dubasdey@users.noreply.github.com>
2016-11-21 17:18:35 +0100

    Changes to use new settings and support R3

--- bz2redmine.rb
+++ bz2redmine.rb
@@ -24,6 +24,9 @@
 # EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 
+# For Redmine 3 or upper (changed email update)
+# 
+
 require "rubygems"
 require "mysql2"
 require File.expand_path(File.join(File.dirname(__FILE__), "settings"))
@@ -35,12 +38,14 @@ class ConnectionInfo
   attr_accessor :user
   attr_accessor :password
   attr_accessor :dbname
-
-  def initialize(host, user, password, dbname)
+  attr_accessor :encoding
+  
+  def initialize(host, user, password, dbname,encoding)
     @host = host
     @user = user
     @password = password
     @dbname = dbname
+	@encoding = encoding
   end
 end
 
@@ -49,8 +54,8 @@ class BugzillaToRedmine
 
 # Startup
   def initialize
-    @bugzillainfo =  ConnectionInfo.new(BUGZILLA_HOST, BUGZILLA_USER, BUGZILLA_PASSWORD, BUGZILLA_DB)
-    @redmineinfo = ConnectionInfo.new(REDMINE_HOST, REDMINE_USER, REDMINE_PASSWORD, REDMINE_DB)
+    @bugzillainfo =  ConnectionInfo.new(BUGZILLA_HOST, BUGZILLA_USER, BUGZILLA_PASSWORD, BUGZILLA_DB, BUGZILLA_ENCODING)
+    @redmineinfo = ConnectionInfo.new(REDMINE_HOST, REDMINE_USER, REDMINE_PASSWORD, REDMINE_DB, REDMINE_ENCODING)
 
     # Bugzilla priority to Redmine priority map
     @issuePriorities = ISSUE_PRIORITIES
@@ -61,29 +66,45 @@ class BugzillaToRedmine
     # Bugzilla status to Redmine status map
     @issueStatus = ISSUE_STATUS
 
+	# User default lang
+	@defaultLanguage = REDMINE_DEFAULT_LANG
+	
     # create hashcode for default password
     @passwordSalt = "" 
     @defaultPassword = Digest::SHA1::hexdigest(@passwordSalt+Digest::SHA1::hexdigest(REDMINE_DEFAULT_USER_PASSWORD))
   end
 
-  # Process Guide 
+  # Process Guide
+  # If process fail in one of the define blocks you could comment the previous blocks 
+  # and launch again to continue
   def migrate
     self.open_connections
     self.perform_sanity_checks
-    self.clear_redmine_tables
-    self.migrate_projects
-    self.migrate_versions
-    self.migrate_users
-    self.migrate_groups
-    self.migrate_members
-    self.migrate_member_roles
-    self.migrate_groups_users
-    self.migrate_categories
+	
+	# BLOCK: Base, projects and versions
+    # self.clear_redmine_tables
+    # self.migrate_projects
+    # self.migrate_versions
+	
+	# BLOCK: Uses
+	#self.clear_redmine_users
+    #self.migrate_users
+    #self.migrate_groups
+    #self.migrate_members
+    #self.migrate_member_roles
+    #self.migrate_groups_users
+	
+	# BLOCK: Issues
+	#self.migrate_categories
     self.migrate_issues
     self.migrate_time_entries
     self.migrate_watchers
     self.migrate_issue_relations
+	
+	# BLOCK: Attachments
     self.migrate_attachments
+	
+	# End
     self.close_connections
   end
 
@@ -92,7 +113,7 @@ class BugzillaToRedmine
     puts s
   end
 
-  # Mysql Operations
+# Mysql Operations
   def open_connection(info)
     self.log "opening #{info.inspect}"
 	db = Mysql2::Client.new(
@@ -100,7 +121,7 @@ class BugzillaToRedmine
 		:username=>info.user,
 		:password=>info.password, 
 		:database=>info.dbname,
-		:encoding=>'utf8',
+		:encoding=>info.encoding,
 		:reconnect=>true
 	)
     return db
@@ -111,7 +132,6 @@ class BugzillaToRedmine
     statement = @bugzilladb.prepare(sql)
 	results = statement.execute(*args)
 	results.each(:as => :array) do |row|
-		#self.log("Bugzilla row: #{row}");
 		yield row
 	end
 	statement.close()
@@ -129,15 +149,19 @@ class BugzillaToRedmine
     statement = @redminedb.prepare(sql)
 	results = statement.execute(*args)
 	results.each(:as => :array) do |row|
-		#self.log("Redmine row: #{row}");
 		yield row
 	end
     statement.close()
   end
   
+  def red_check_exists(sql, *args)
+    self.red_select_sql(sql, *args) { |i|  return true }
+    return false
+  end
 
-  # Migration operations in migrate process (view migrate funcion)
+# Migration operations in migrate process (view migrate funcion)
   def open_connections
+    self.log("Opening connections")
     @bugzilladb = self.open_connection(@bugzillainfo)
     @redminedb = self.open_connection(@redmineinfo)
   end
@@ -148,7 +172,6 @@ class BugzillaToRedmine
     @redminedb.close
   end
   
-
   def clear_redmine_tables
     sqls = [
       "DELETE FROM projects",
@@ -172,16 +195,29 @@ class BugzillaToRedmine
     end
   end
 
+  def clear_redmine_users
+    ["DELETE FROM users",
+      "DELETE FROM email_addresses",
+      "DELETE FROM user_preferences",
+      "DELETE FROM members",
+      "DELETE FROM member_roles",
+      "DELETE FROM groups_users",
+      "DELETE FROM messages",
+      "DELETE FROM tokens",
+      "DELETE FROM watchers"].each do |sql|
+      self.red_exec_sql(sql)
+    end
+  end
+  
   def migrate_projects
     tree_idx = 1
     self.bz_select_sql("SELECT products.id, products.name, products.description, products.classification_id, classifications.name as classification_name FROM products, classifications WHERE products.classification_id = classifications.id order by products.name") do |row|
-      identifier = row["name"].downcase # 1
-      status = row["classification_id"] == 1 ? 9 : 1 # 3
-	  
+      identifier = row["name"].downcase
+      status = row["classification_id"] == 1 ? 9 : 1
       created_at = self.find_min_created_at_for_product(row["id"])
       updated_at = self.find_max_bug_when_for_product(row["id"])
       self.red_exec_sql("INSERT INTO projects (id, name, description, is_public, identifier, created_on, updated_on, status, lft, rgt) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
-			row["id"], row["name"], row["description"], 1, identifier,created_at, updated_at, status, tree_idx, tree_idx+1)
+      row["id"], row["name"], row["description"], 1, identifier,created_at, updated_at, status, tree_idx, tree_idx+1)
       tree_idx = tree_idx + 2
       tree_idx = tree_idx + 2
       self.insert_project_trackers(row["id"])
@@ -191,8 +227,7 @@ class BugzillaToRedmine
 
   def find_min_created_at_for_product(product_id)
     bug_when = '1970-01-01 10:22:25'
-    sql = "select min(b.creation_ts) as ct from products p join bugs b on b.product_id = p.id where product_id=?"
-    self.bz_select_sql(sql, product_id) do |row|
+    self.bz_select_sql("select min(b.creation_ts) as ct from products p join bugs b on b.product_id = p.id where product_id=?", product_id) do |row|
       bug_when = row["ct"]
     end
     return bug_when
@@ -215,17 +250,6 @@ class BugzillaToRedmine
   end
 
   def migrate_users
-    ["DELETE FROM users",
-      "DELETE FROM user_preferences",
-      "DELETE FROM members",
-      "DELETE FROM member_roles",
-      "DELETE FROM groups_users",
-      "DELETE FROM messages",
-      "DELETE FROM tokens",
-      "DELETE FROM watchers"].each do |sql|
-      self.red_exec_sql(sql)
-    end
-
     if not REDMINE_DEFAULT_AUTH_SOURCE_ID.nil?
       ldap = Net::LDAP.new :host => REDMINE_LDAP['host'], # your LDAP host name or IP goes here,
         :port => REDMINE_LDAP['port'], # your LDAP host port goes here,
@@ -234,8 +258,7 @@ class BugzillaToRedmine
           :method => :simple,
           :username => REDMINE_LDAP['bind_user'], # a user w/sufficient privileges to read from AD goes here,
           :password => REDMINE_LDAP['bind_pass'] # the user's password goes here
-        }	
-
+        }
       raise "LDAP bind failed." unless ldap.bind
     end
 
@@ -244,7 +267,8 @@ class BugzillaToRedmine
       login_name = row["login_name"]
       real_name = row["realname"]
       disabled_text = row["disabledtext"]
-	  extern_id = row["extern_id"]
+      extern_id = row["extern_id"]
+      email = row["login_name"]
       if real_name.nil?
         (last_name, first_name) = ['empty', 'empty']
       else
@@ -259,19 +283,21 @@ class BugzillaToRedmine
 
       status = disabled_text.to_s.strip.empty? ? 1 : 3
 
+      # Extern LDAP
       if not extern_id.nil? and not REDMINE_DEFAULT_AUTH_SOURCE_ID.nil?
-		search_filter = Net::LDAP::Filter.eq(REDMINE_LDAP['email_attr'], login_name)
+        search_filter = Net::LDAP::Filter.eq(REDMINE_LDAP['email_attr'], login_name)
         self.log("Searching LDAP for %s" % login_name)
         result = ldap.search(:filter => search_filter, :attributes => [REDMINE_LDAP['login_attr']], :return_result => true) do |user|
           self.log("User found in LDAP")
-          self.red_exec_sql("INSERT INTO users (id, login, mail, firstname, lastname, language, mail_notification, status, type, auth_source_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-            user_id, user[REDMINE_LDAP['login_attr']].first, login_name, last_name, first_name, 'en', 'only_my_events', status, 'User', REDMINE_DEFAULT_AUTH_SOURCE_ID)
-		end
+          self.red_exec_sql("INSERT INTO users (id, login, firstname, lastname, language, mail_notification, status, type, auth_source_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            user_id, user[REDMINE_LDAP['login_attr']].first, last_name, first_name, @defaultLang, 'only_my_events', status, 'User', REDMINE_DEFAULT_AUTH_SOURCE_ID)
+        end
       end
 
+      # Local
       if result.nil? or extern_id.nil?
-        self.red_exec_sql("INSERT INTO users (id, login, mail, firstname, lastname, language, mail_notification, status, hashed_password, type, salt) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-          user_id, login_name, login_name, last_name, first_name, 'en', 'only_my_events', status, @defaultPassword, 'User', @passwordSalt)
+        self.red_exec_sql("INSERT INTO users (id, login, firstname, lastname, language, mail_notification, status, hashed_password, type, salt) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+          user_id, login_name, last_name, first_name, @defaultLang, 'only_my_events', status, @defaultPassword, 'User', @passwordSalt)
       end
 
       other = """---
@@ -279,6 +305,9 @@ class BugzillaToRedmine
 :no_self_notified: true
       """
       self.red_exec_sql("INSERT INTO user_preferences (user_id,others) values (?, ?)", user_id, other)
+
+      # Add Email
+      self.red_exec_sql("INSERT INTO email_addresses(user_id,address,is_default,notify) VALUES(?,?,1,1)",user_id,email)
     end
   end
   
@@ -286,7 +315,7 @@ class BugzillaToRedmine
     self.bz_select_sql("select name from groups") do |row|
       name = row["name"]
       self.red_exec_sql("insert into users (lastname, mail_notification, admin, status, type, language) values (?, ?, ?, ?, ?, ?)",
-        name, 'only_my_events', (name == 'admin' ? 1 : 0) , 1, 'Group', 'en')
+        name, 'only_my_events', (name == 'admin' ? 1 : 0) , 1, 'Group', @defaultLang)
     end
   end
 
@@ -320,7 +349,7 @@ class BugzillaToRedmine
     end
   end
 
-  def insert_custom_fields
+  def migrate_custom_fields
     self.red_exec_sql("delete from custom_fields")
     self.red_exec_sql("delete from custom_fields_trackers")
     self.red_exec_sql("delete from custom_values")
@@ -368,58 +397,37 @@ class BugzillaToRedmine
   def migrate_issues
     self.red_exec_sql("delete from issues")
     self.red_exec_sql("delete from journals")
-    self.insert_custom_fields
-    sql = "SELECT bugs.bug_id,
-               bugs.assigned_to,
-               bugs.bug_status,
-               bugs.creation_ts,
-               bugs.short_desc,
-               bugs.product_id,
-               bugs.reporter,
-               bugs.version,
-               bugs.resolution,
-               bugs.estimated_time,
-               bugs.remaining_time,
-               bugs.deadline,
-               bugs.target_milestone,
-               bugs.bug_severity,
-               bugs.priority,
-               bugs.component_id,
-               bugs.status_whiteboard AS whiteboard,
-               bugs.bug_file_loc AS url,
-               longdescs.comment_id,
-               longdescs.thetext,
-               longdescs.bug_when,
-               longdescs.who,
-               longdescs.isprivate
-           FROM bugs, longdescs
-           WHERE bugs.bug_id = longdescs.bug_id
-           ORDER BY bugs.bug_id, longdescs.bug_when"
+    self.migrate_custom_fields
+    sql = "SELECT b.bug_id, b.assigned_to, b.bug_status, b.creation_ts, b.short_desc, b.product_id,
+               b.reporter, b.version, b.resolution, b.estimated_time, b.remaining_time,
+               b.deadline, b.target_milestone, b.bug_severity, b.priority, b.component_id,
+               b.status_whiteboard,b.bug_file_loc,ld.comment_id,ld.thetext,ld.bug_when,ld.who,
+               ld.isprivate FROM bugs b, longdescs ld WHERE b.bug_id = ld.bug_id ORDER BY b.bug_id, ld.bug_when"
     current_bug_id = -1
     self.bz_select_sql(sql) do |row|
-      ( bug_id,
-        assigned_to,
-        bug_status,
-        creation_ts,
-        short_desc,
-        product_id,
-        reporter,
-        version,
-        resolution,
-        estimated_time,
-        remaining_time,
-        deadline,
-        target_milestone,
-        bug_severity,
-        priority,
-        component_id,
-        whiteboard,
-        url,
-        comment_id,
-        thetext,
-        bug_when,
-        who,
-        isprivate) = row
+        bug_id = row["bug_id"]
+        assigned_to = row["assigned_to"]
+        bug_status = row["bug_status"]
+        creation_ts = row["creation_ts"]
+        short_desc = row["short_desc"]
+        product_id = row["product_id"]
+        reporter= row["reporter"]
+        version= row["version"]
+        resolution= row["resolution"]
+        estimated_time= row["estimated_time"]
+        remaining_time= row["remaining_time"]
+        deadline= row["deadline"]
+        target_milestone= row["target_milestone"]
+        bug_severity= row["bug_severity"]
+        priority= row["priority"]
+        component_id= row["component_id"]
+        whiteboard= row["status_whiteboard"]
+        url= row["bug_file_loc"]
+        comment_id = row["comment_id"]
+        thetext = row["thetext"]
+        bug_when= row["bug_when"]
+        who= row["who"]
+        isprivate = row["isprivate"]
       if(current_bug_id != bug_id)
         sql = "INSERT INTO issues (id, project_id, subject, description, assigned_to_id, author_id, created_on, updated_on, start_date, due_date, done_ratio, estimated_hours, priority_id, fixed_version_id, category_id, tracker_id, status_id, root_id, lft, rgt) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
         target_milestone_id = self.find_version_id(product_id, version)
@@ -430,8 +438,7 @@ class BugzillaToRedmine
         done_ratio = estimated_time.to_f.abs < 1e-3 ? 0.00 : ((estimated_time.to_f - remaining_time.to_f)/estimated_time.to_f)*100
         self.red_exec_sql(sql, bug_id, product_id, short_desc, thetext, assigned_to, reporter, creation_ts,  updated_at, creation_ts, deadline, done_ratio, estimated_time, priority_id, target_milestone_id, component_id, tracker_id, status_id, bug_id, 1, 2)
         current_bug_id = bug_id
-        self.red_exec_sql("INSERT INTO custom_values (customized_type, customized_id, custom_field_id, value)  VALUES (?, ?, ?, ?)", 
-			'Issue', bug_id, 1, url)
+        self.red_exec_sql("INSERT INTO custom_values (customized_type, customized_id, custom_field_id, value)  VALUES (?, ?, ?, ?)", 'Issue', bug_id, 1, url)
       else
         self.red_exec_sql("INSERT INTO journals (id, journalized_id, journalized_type, user_id, notes, created_on)  VALUES (?, ?, ?, ?, ?, ?)", 
 			comment_id, bug_id, "Issue", who, thetext, bug_when)
@@ -469,13 +476,21 @@ class BugzillaToRedmine
 
   def migrate_attachments
     self.red_exec_sql("DELETE FROM attachments")
-    sql = "SELECT attachments.attach_id, attachments.bug_id, attachments.filename, attachments.mimetype, attachments.submitter_id, attachments.creation_ts, attachments.description, attach_data.thedata FROM attachments, attach_data WHERE attachments.attach_id = attach_data.id"
+    sql = "SELECT a.attach_id, a.bug_id, a.filename, a.mimetype, a.submitter_id, a.creation_ts, a.description, ad.thedata FROM attachments a, attach_data ad WHERE a.attach_id = ad.id"
     self.bz_select_sql(sql) do |row|
-      (attach_id, bug_id, filename, mimetype, submitter_id, creation_ts, description, thedata ) = row
+      attach_id = row["attach_id"]
+	  bug_id = row["bug_id"]
+	  filename = row["filename"] 
+	  mimetype = row["mimetype"]  
+	  submitter_id= row["submitter_id"]  
+	  creation_ts= row["creation_ts"] 
+	  description= row["description"]  
+	  thedata = row["thedata"]  
       disk_filename = self.get_disk_filename(attach_id, filename)
       filesize = thedata.size()
       sql = "INSERT INTO attachments (id, container_id, container_type, filename, filesize, disk_filename, content_type, digest, downloads, author_id, created_on, description) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
       self.red_exec_sql(sql, attach_id, bug_id, 'Issue', filename, filesize, disk_filename, mimetype, '', 0, submitter_id, creation_ts, description)
+	  self.log("Writting #{ATTACHMENT_PATH}/#{disk_filename}")
       File.open("#{ATTACHMENT_PATH}/#{disk_filename}", "wb") do |f|
         f.write(thedata)
       end
@@ -517,9 +532,9 @@ class BugzillaToRedmine
 
   def select_last_insert_id
     result = 0
-    red_select_sql("select last_insert_id()") do |row|
+    red_select_sql("select last_insert_id() as ct") do |row|
 		self.log("LAST_INSERT_ID: #{row}")
-      result = row[0]
+      result = row["ct"]
     end
     result
   end
@@ -559,14 +574,7 @@ class BugzillaToRedmine
   end
 
   def insert_project_modules(project_id)
-    ['issue_tracking',
-      'time_tracking',
-      'news',
-      'documents',
-      'files',
-      'wiki',
-      'repository',
-      'boards',].each do |m|
+    REDMINE_ENABLED_MODULES.each do |m|
       self.red_exec_sql("INSERT INTO enabled_modules (project_id, name) VALUES (?, ?)", project_id, m)
     end
   end
@@ -635,11 +643,6 @@ class BugzillaToRedmine
     if count > 0
       throw "there are bug priorities, which cannot be mapped. please modify the ISSUE_TRACKERS in settings.rb accordingly."
     end
-  end
-
-  def red_check_exists(sql, *args)
-    self.red_select_sql(sql, *args) { |i|  return true }
-    return false
   end
 
   def verify_trackers
